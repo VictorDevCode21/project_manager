c8539ba655aa17093e2782a42103129f5c852a85
ricardo <r.fernandez@correo.unimet.edu.ve>
2025-10-19 00:50:40 -0400

    update on profile and controller creation
    falta el controller

---
+++ lib/controllers/profile_controller.dart
@@ -0,0 +1,86 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:prolab_unimet/services/auth_service.dart';
+import 'package:go_router/go_router.dart';
+import '../providers/auth_provider.dart';
+
+//mañana
+
+class ProfileController {
+  final newnameController = TextEditingController();
+  final newemailController = TextEditingController();
+  final newphoneController = TextEditingController();
+  final newpasswordController = TextEditingController();
+  final newpersonIdController = TextEditingController();
+
+  DateTime? selectedDate;
+  final formKey = GlobalKey<FormState>();
+  final AuthService _authService = AuthService();
+
+  String? validateName(String? value) {
+    if (value == null || value.trim().isEmpty) {
+      return 'Por favor, ingresa tu nombre';
+    }
+    return null;
+  }
+
+  String? validatePersonId(String? value) {
+    if (value == null || value.trim().isEmpty) {
+      return "Por favor, ingresa tu cédula";
+    }
+    return null;
+  }
+
+  String? validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Ingresa tu correo electrónico';
+    }
+
+    // Allow only UNIMET institutional emails
+    final regex = RegExp(
+      r'^[\w\.-]+@(correo\.unimet\.edu\.ve|unimet\.edu\.ve)$',
+      caseSensitive: false,
+    );
+
+    if (!regex.hasMatch(value.trim())) {
+      return 'Solo se permiten correos institucionales de la UNIMET';
+    }
+
+    return null;
+  }
+
+  String? validatePhone(String? value) {
+    if (value == null || value.isEmpty) return 'Ingresa tu número de teléfono';
+    if (!RegExp(r'^\d{7,11}$').hasMatch(value)) return 'Número inválido';
+    return null;
+  }
+
+  String? validatePassword(String? value) {
+    if (value == null || value.isEmpty) return 'Ingresa una contraseña';
+    if (value.length < 6) return 'Debe tener al menos 6 caracteres';
+    return null;
+  }
+
+  String? validateDate() =>
+      selectedDate == null ? 'Selecciona una fecha' : null;
+
+  bool validateForm() {
+    final valid = formKey.currentState?.validate() ?? false;
+    return valid != null && selectedDate != null;
+  }
+
+  /*Future<User> modifyUser({
+    required String name,
+    required String email,
+    required String password,
+    required String phoneNumber,
+    required String role,
+    required DateTime birthDate,
+    required String personId,
+  }) async {
+    var user = FirebaseAuth.instance.currentUser;
+    
+    
+  }*/
+}
--- lib/views/profile_page.dart
+++ lib/views/profile_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:prolab_unimet/views/layouts/admin_layout.dart';
+import 'package:prolab_unimet/widgets/custom_text_field_widget.dart';
+import 'package:prolab_unimet/controllers/register_controller.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
@@ -8,8 +10,23 @@ class ProfileView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          TextButton.icon(
+            onPressed: () {},
+            icon: Icon(Icons.arrow_back, color: Colors.black),
+            label: Text(
+              'Volver al dashboard',
+              style: TextStyle(color: Colors.black, fontSize: 18),
+            ),
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+            ),
+          ),
           Text(
             'Modificar Perfil',
             textAlign: TextAlign.left,
@@ -24,6 +41,7 @@ class ProfileView extends StatelessWidget {
             'Actualiza tu información personal y datos de contacto',
             textAlign: TextAlign.left,
           ),
+          SizedBox(height: 40),
           ProfileManager(),
         ],
       ),
@@ -57,15 +75,20 @@ class ComeBackButton extends StatelessWidget {
   }
 }
 
-class ProfileManager extends StatelessWidget {
+class ProfileManager extends StatefulWidget {
   const ProfileManager({super.key});
 
+  @override
+  State<ProfileManager> createState() => _ProfileManagerState();
+}
+
+class _ProfileManagerState extends State<ProfileManager> {
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        width: 800,
-        padding: EdgeInsets.all(5),
+        width: 900,
+        padding: EdgeInsets.all(20),
         decoration: BoxDecoration(
           border: Border.all(color: Colors.cyanAccent),
           borderRadius: BorderRadius.circular(10),
@@ -75,8 +98,15 @@ class ProfileManager extends StatelessWidget {
           children: [
             Row(
               children: [
-                Icon(Icons.person, color: Colors.indigo),
-                Text('Informacion personal'),
+                Icon(Icons.person_2_outlined, color: Colors.indigo),
+                Text(
+                  'Informacion personal',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.indigo,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
               ],
             ),
             Row(
@@ -84,19 +114,132 @@ class ProfileManager extends StatelessWidget {
                 Text('Actualiza tu información personal y datos de contacto'),
               ],
             ),
+            const SizedBox(height: 20),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 IconButton(
                   onPressed: () {},
-                  icon: Icon(Icons.person),
+                  icon: Icon(Icons.account_circle),
                   color: Colors.indigo,
-                  iconSize: 50,
+                  iconSize: 80,
                 ),
               ],
             ),
+            Form(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(child: column1()),
+                  SizedBox(width: 30),
+                  Expanded(child: column2()),
+                ],
+              ),
+            ),
+            descriptionBox(),
+            SizedBox(height: 10),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [cancel1(), SizedBox(width: 10), saveChanges()],
+            ),
+            SizedBox(height: 10),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget column1() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Nombre completo'),
+        CustomTextField(
+          labelText: 'nombre',
+          hintText: 'Tu nombre completo',
+          iconData: Icons.person,
+        ),
+        SizedBox(height: 10),
+        Text('Correo electronico'),
+        CustomTextField(
+          labelText: 'correo',
+          hintText: 'tu@email.com',
+          iconData: Icons.mail,
+        ),
+        SizedBox(height: 10),
+        Text('Telefono'),
+        CustomTextField(
+          labelText: 'telefono',
+          hintText: '+58 123 456 789',
+          iconData: Icons.phone,
+        ),
+      ],
+    );
+  }
+
+  Widget column2() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Contraseña'),
+        CustomTextField(
+          labelText: 'nombre',
+          hintText: '123456789',
+          iconData: Icons.password,
+        ),
+        SizedBox(height: 10),
+        Text('Cedula'),
+        CustomTextField(
+          labelText: 'cedula',
+          hintText: 'Numero de cedula',
+          iconData: Icons.info,
+        ),
+        SizedBox(height: 10),
+        Text('Fecha de nacimiento'),
+        TextFormField(
+          readOnly: true,
+          onTap: () async {
+            final pickedDate = await showDatePicker(
+              context: context,
+              firstDate: DateTime(1950),
+              initialDate: DateTime(2000),
+              lastDate: DateTime.now(),
+            );
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget descriptionBox() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        border: Border(bottom: BorderSide(color: Colors.cyan)),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [Text('Descripcion personal'), TextField()],
+      ),
+    );
+  }
+
+  Widget cancel1() {
+    return OutlinedButton(onPressed: () {}, child: Text('Cancelar'));
+  }
+
+  Widget saveChanges() {
+    return TextButton.icon(
+      onPressed: () {},
+      icon: Icon(Icons.save, color: Colors.white),
+      label: Text(
+        'Guardar cambios',
+        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+      ),
+      style: TextButton.styleFrom(
+        backgroundColor: Colors.indigo,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        padding: const EdgeInsets.symmetric(horizontal: 16),
       ),
     );
   }
