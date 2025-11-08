fbc94fa942e47984a6ca8740e2d519f7ddf9dac3
ChantalPT <chantyspinto@gmail.com>
2025-11-07 22:56:20 -0400

    Page Header (title, bottons, etc).

--- lib/views/task_view.dart
+++ lib/views/task_view.dart
@@ -21,30 +21,116 @@ class _TaskView extends State<TaskView> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Column(
+                  Wrap(
+                    spacing: 20,
+                    crossAxisAlignment: WrapCrossAlignment.center,
                     children: [
-                      Text(
-                        'Gestión de tareas',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xff1b5bf5),
+                      TextButton.icon(
+                        onPressed: () {},
+                        icon: Icon(Icons.arrow_back, color: Colors.grey),
+                        label: Text(
+                          'Volver',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        style: TextButton.styleFrom(
+                          //backgroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          padding: const EdgeInsets.symmetric(horizontal: 8),
                         ),
                       ),
-                      SizedBox(height: 4),
-                      Text(
-                        'Tablero para gestionar tareas de proyectos',
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.grey,
-                        ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Gestión de Tareas',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xff1b5bf5),
+                            ),
+                          ),
+                          SizedBox(height: 4),
+                          Text(
+                            'Tablero para gestionar tareas de proyectos',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                  Text(
-                    '+ Nueva Columna',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w800),
+                  Wrap(
+                    spacing: 16,
+                    children: [
+                      ElevatedButton.icon(
+                        onPressed: () {
+                          //crear
+                        },
+                        icon: const Icon(
+                          Icons.add,
+                          size: 18,
+                          color: Color(0xff38465a),
+                        ),
+                        label: const Text(
+                          'Nueva Columna',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xff38465a),
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(
+                            255,
+                            224,
+                            228,
+                            231,
+                          ),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 14,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                      ),
+                      ElevatedButton.icon(
+                        onPressed: () {
+                          //crear tareas
+                        },
+                        icon: const Icon(
+                          Icons.add,
+                          size: 18,
+                          color: Colors.white,
+                        ),
+                        label: const Text(
+                          'Nueva Tarea',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xff2d55fa),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 14,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
